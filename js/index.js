a292ddc05d378297c6d925c36125ac17c30c4c20
aricycle7 <148219989+aricycle7@users.noreply.github.com>
2024-04-19 10:33:16 -0400

    xsdwecfrgthyjmuk

--- js/index.js
+++ js/index.js
@@ -22,39 +22,72 @@ let retreat2 = {
   cost: 40,
 };
 
-let cost1 = retreat1.cost * document.getElementById("day_value".value);
-let cost2 = retreat2.cost * 50;
+let cost1 = retreat1.cost * document.getElementById("day_input").value;
+let cost2 = retreat2.cost * document.getElementById('day_input').value;
 
-if(document.getElementById('txt_input').value == "mountain retreat"){
-    let elName = document.getElementById("retreat");
-elName.innerText = retreat1.name;
-let elImage = document.getElementById("image");
-elImage.innerHTML =
-  '<img src="../res/Log_cabin_in_Stabbursdalen_Resort,_Porsanger,_Troms_og_Finnmark,_Norway,_2022_August.jpg" width = 500px>';
-let el1 = document.getElementById("rooms");
-el1.innerText = "Number of rooms: " + retreat1.rooms;
-let el2 = document.getElementById("scenery");
-el2.innerText = "Scenery: " + retreat1.scenery;
-let el3 = document.getElementById("tents");
-el3.innerText = "Number of Tents: " + retreat1.tents;
-let el4 = document.getElementById("cost");
-el4.innerText = "Cost per night: " + "$" + retreat1.cost;
-let el5 = document.getElementById('total');
-el5.innerText = "Cost For Stay: " + "$" + cost1;
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let monthName = months[new Date('April 1, 24 00:20:18').getMonth()];
+console.log(monthName);
+
+if (monthName == "April") {
+  cost1 = (retreat1.cost * document.getElementById("day_input").value) * 0.80; 
+  cost2 = (retreat2.cost * document.getElementById('day_input').value) * 0.80;
+} else if (monthName == "May") {
+  cost1 = (retreat1.cost * document.getElementById("day_input").value) * 0.90; 
+  cost2 = (retreat2.cost * document.getElementById('day_input').value) * 0.90;
+}
+else{
+   cost1 = retreat1.cost * document.getElementById("day_input").value;
+   cost2 = retreat2.cost * document.getElementById('day_input').value;
+}
+console.log(cost1)
+
+
+if (document.getElementById('txt_input').value == "mountain retreat") {
+  let elName = document.getElementById("retreat");
+  elName.innerText = retreat1.name;
+  let elImage = document.getElementById("image");
+  elImage.innerHTML =
+    '<img src="../res/Log_cabin_in_Stabbursdalen_Resort,_Porsanger,_Troms_og_Finnmark,_Norway,_2022_August.jpg" width = 500px>';
+  let el1 = document.getElementById("rooms");
+  el1.innerText = "Number of rooms: " + retreat1.rooms;
+  let el2 = document.getElementById("scenery");
+  el2.innerText = "Scenery: " + retreat1.scenery;
+  let el3 = document.getElementById("tents");
+  el3.innerText = "Number of Tents: " + retreat1.tents;
+  let el4 = document.getElementById("cost");
+  el4.innerText = "Cost per night: " + "$" + retreat1.cost;
+  let el5 = document.getElementById('total');
+  el5.innerText = "Cost For Stay: " + "$" + cost1;
 }
 
-if(document.getElementById('txt_input').value == "trail getaway"){
-let elName = document.getElementById('retreat');
-elName.innerText = retreat2.name;
-let elImage = document.getElementById('image');
-elImage.innerHTML =
-  '<img src="../res/Camping_Tents_in_the_Woods.jpg" width = 500px>';
+if (document.getElementById('txt_input').value == "trail getaway") {
+  let elName = document.getElementById('retreat');
+  elName.innerText = retreat2.name;
+  let elImage = document.getElementById('image');
+  elImage.innerHTML =
+    '<img src="../res/Camping_Tents_in_the_Woods.jpg" width = 500px>';
   let el1 = document.getElementById('rooms');
-el1.innerText = "Number of rooms: " + retreat2.rooms;
-let el2 = document.getElementById('scenery');
-el2.innerText = "Scenery: " + retreat2.scenery;
-let el3 = document.getElementById('tents');
-el3.innerText = "Number of Tents: " + retreat2.tents;
-let el4 = document.getElementById('cost');
-el4.innerText = "Cost per night: " + "$" + retreat2.cost;
+  el1.innerText = "Number of rooms: " + retreat2.rooms;
+  let el2 = document.getElementById('scenery');
+  el2.innerText = "Scenery: " + retreat2.scenery;
+  let el3 = document.getElementById('tents');
+  el3.innerText = "Number of Tents: " + retreat2.tents;
+  let el4 = document.getElementById('cost');
+  el4.innerText = "Cost per night: " + "$" + retreat2.cost;
+  let el5 = document.getElementById('total');
+  el5.innerText = "Cost For Stay: " + "$" + cost2;
 }
